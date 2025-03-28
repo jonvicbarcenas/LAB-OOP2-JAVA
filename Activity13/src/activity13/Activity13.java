74fef7a4ea094838924e3c4f0d6b30ec36e649c8
LAB12_USER ON WS07 <L12X17W07@citulab.net>
2025-03-28 17:17:36 +0800

    sheesh

---
+++ Activity13/src/activity13/Activity13.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package activity13;
+
+/**
+ *
+ * @author L12X17W07
+ */
+public class Activity13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+    }
+    
+}
